a3b21f377c09ca175ea0309d8922a8078f2e2a7c
BalajiM012 <113889348+BalajiM012@users.noreply.github.com>
2026-04-28 14:20:43 +0530

    Implement email sending functionality

---
+++ services/email_service.py
@@ -0,0 +1,10 @@
+from flask_mail import Message
+from app import mail
+
+def send_email(to, subject, body):
+    msg = Message(
+        subject=subject,
+        recipients=[to]
+    )
+    msg.body = body
+    mail.send(msg)
