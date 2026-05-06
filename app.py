3ee617b149a7819397190c89a058f38e75460653
BalajiM012 <113889348+BalajiM012@users.noreply.github.com>
2026-05-06 22:18:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from routes.auth_routes import auth_bp
 from routes.patient_routes import patient_bp
 from routes.doctor_routes import doctor_bp
 from routes.admin_routes import admin_bp
-
+from routes.record_routes import record_bp
+app.register_blueprint(record_bp)
 app = Flask(__name__)
 app.secret_key = "secret"
 
