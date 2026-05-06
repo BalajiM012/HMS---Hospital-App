116a12cef8d75351cb5648b1f43e444d78fed28e
BalajiM012 <113889348+BalajiM012@users.noreply.github.com>
2026-05-06 22:11:52 +0530

    Add doctor routes for dashboard and status update

---
+++ routes/doctor_routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, session, redirect
+from extensions.db import cursor, db
+from utils.decorators import role_required
+
+doctor_bp = Blueprint('doctor', __name__)
+
+@doctor_bp.route('/doctor')
+@role_required('doctor')
+def dashboard():
+    cursor.execute("""
+        SELECT a.*, u.name AS patient_name
+        FROM appointments a
+        JOIN users u ON a.patient_id = u.id
+        WHERE doctor_id=%s
+    """, (session['user_id'],))
+
+    data = cursor.fetchall()
+    return render_template('doctor/dashboard.html', data=data)
+
+
+@doctor_bp.route('/update_status/<int:id>/<status>')
+@role_required('doctor')
+def update_status(id, status):
+    cursor.execute("UPDATE appointments SET status=%s WHERE id=%s", (status, id))
+    db.commit()
+    return redirect('/doctor')
