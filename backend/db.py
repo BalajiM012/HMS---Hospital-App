1dc9e91725cac89dd48bd234e15770639f431b84
BalajiM012 <113889348+BalajiM012@users.noreply.github.com>
2026-05-11 09:59:54 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,5 @@
 import pymysql
 from flask import g
-import os
 
 _app = None
 
@@ -11,15 +10,15 @@ def init_db(app):
 def get_db():
     if 'db' not in g:
         g.db = pymysql.connect(
-    host=_app.config['MYSQL_HOST'],
-    port=_app.config['MYSQL_PORT'],
-    user=_app.config['MYSQL_USER'],
-    password=_app.config['MYSQL_PASSWORD'],
-    database=_app.config['MYSQL_DB'],
-    cursorclass=pymysql.cursors.DictCursor,
-    autocommit=False,
-    ssl={"ssl": {}}
-)
+            host=_app.config['MYSQL_HOST'],
+            port=_app.config['MYSQL_PORT'],
+            user=_app.config['MYSQL_USER'],
+            password=_app.config['MYSQL_PASSWORD'],
+            database=_app.config['MYSQL_DB'],
+            cursorclass=pymysql.cursors.DictCursor,
+            autocommit=False,
+            ssl={"ssl": {}}
+        )
     return g.db
 
 def close_db(e=None):
@@ -31,8 +30,12 @@ def query(sql, args=(), one=False, commit=False):
     db = get_db()
     cur = db.cursor()
     cur.execute(sql, args)
+
     if commit:
         db.commit()
         return cur.lastrowid
+
     rv = cur.fetchone() if one else cur.fetchall()
+    cur.close()
+
     return rv
